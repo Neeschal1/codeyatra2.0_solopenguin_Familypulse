cf40cb990e03496daf7e400f213eceae74e469dd
Neeschal1 <neeschalpok04@gmail.com>
2026-02-23 00:52:42 +0545

    updated requirements for backend

--- backend/apps/reports/models/entities.py
+++ backend/apps/reports/models/entities.py
@@ -1,3 +1,7 @@
 from django.db import models
+from ...users.models import User
 
-# Create your models here.
+class Report(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    weight = models.CharField(max_length=10)
+    date_time = models.DateTimeField(auto_now_add=True)
