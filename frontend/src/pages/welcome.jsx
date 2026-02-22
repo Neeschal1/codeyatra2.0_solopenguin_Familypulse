675e7a8f12532f4d7520ef6989604a65d7aaebaa
avishekbasnet <avishekbasnet619@gmail.com>
2026-02-22 18:07:52 +0545

    handled login navigation issue

--- frontend/src/pages/welcome.jsx
+++ frontend/src/pages/welcome.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
+  const navigation = useNavigate()
+
+  const handleLogin = () => {
+    navigation("/login")
+  }
+
+
+  const handleSignup = () => {
+    navigation("/signup")
+  }
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 px-4">
 
@@ -23,11 +35,11 @@ const Welcome = () => {
 
       <div className="w-full max-w-sm space-y-4">
 
-        <button className="w-full border-2 border-blue-600 text-blue-600 py-3 rounded-lg text-lg font-semibold transition hover:bg-black duration-400 hover:text-white">
+        <button onClick={handleLogin} className="w-full border-2 border-blue-600 text-blue-600 py-3 rounded-lg text-lg font-semibold transition hover:bg-black duration-400 hover:text-white">
           Login
         </button>
 
-        <button className="w-full bg-blue-600 text-white py-3 rounded-lg text-lg font-semibold border hover:bg-blue-800 duration-400 border-gray-300  transition">
+        <button onClick={handleSignup} className="w-full bg-blue-600 text-white py-3 rounded-lg text-lg font-semibold border hover:bg-blue-800 duration-400 border-gray-300  transition">
           Sign Up
         </button>
 
