131bc95d8888b0cbf53aec3d39db0c469ac1d6c1
Neeschal1 <neeschalpok04@gmail.com>
2026-02-23 03:21:48 +0545

    frontend git issue solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,27 @@
-import { useState } from 'react'
-import ReactDOM from "react-dom/client";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
-import Login from './pages/login';
-import Signup from './pages/signup';
-import Home from './pages/home';
-import Welcome from './pages/welcome';
-import Dashboard from './pages/dashboard';
+import "./App.css";
 
-<<<<<<< HEAD
-=======
-// import Navbar from './pages/navbar';
-
->>>>>>> origin/main
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Home from "./pages/home";
+import Welcome from "./pages/welcome";
+import Dashboard from "./pages/dashboard";
 
 function App() {
-
   return (
     <Router>
-      <div className="flex w-full h-screen justify-center items-center flex-1 bg-[#EEF3F9]">
+      <div className="flex w-full h-screen justify-center items-center bg-[#EEF3F9]">
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
-<<<<<<< HEAD
           <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-=======
-          <Route path="/signup" element={<Signup />} />         
           <Route path="/home" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-        {/* <Navbar /> */}
->>>>>>> origin/main
-        <welcome />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
