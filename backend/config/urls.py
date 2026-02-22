bda12c7f0610ea1d8af3fc22cf0d925510eadd9d
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-02-22 23:32:33 +0545

    fix: resolve merge conflicts in urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -8,20 +8,15 @@ from drf_spectacular.views import (
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("admin/", admin.site.urls),
-    path("api/users/", include("apps.users.urls")),
     path('payments/', include('apps.payment.api.urls')),
     
     
-=======
     path('admin/', admin.site.urls),
 
     path('api/users/', include('apps.users.urls')),
     path("api/dependents/", include("apps.dependents.urls")),
     path("api/", include("apps.visits.urls")),
 
->>>>>>> dd670d0dbc3e69014d1ef223315182ab7145325a
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/docs/",
