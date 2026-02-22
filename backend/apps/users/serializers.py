cb1cf75ec0083c43170f35fd12c1ffedae11179a
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-02-22 18:36:38 +0545

    Save local work before pull

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -5,8 +5,7 @@ User = get_user_model()
 
 class UserSignupSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=6)
-    # role = serializers.ChoiceField(choices=User.ROLE_CHOICES.choices, default=User.ROLE_CHOICES.USER)
-
+    # role = serializers.ChoiceField(choices=ROLE_CHOICES, default="USER")
     class Meta:
         model = User
         fields = ["email", "password", "role"]
