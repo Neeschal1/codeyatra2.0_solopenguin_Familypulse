a1a9d4517b2f3019e9098e0e5f66564494a110a3
Ashraya Bashyal <ashrayabashyal1@gmail.com>
2026-02-22 20:03:37 +0545

    add serializers for Visits

---
+++ backend/apps/visits/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Visit
+
+class VisitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Visit
+        fields = "__all__"
+        read_only_fields = ["id", "status", "completed_at"]
